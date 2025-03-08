42f6ef78608eb1b1e1333f29fc021b703c52ede6
Tal Mendelson <tal.mendelson@bluesnap.com>
2025-03-08 21:16:10 +0200

    Practice 2-3 added as well.

---
+++ Homework/Chapters/Practice_1.py
@@ -0,0 +1,38 @@
+#################### /ᐠ｡ꞈ｡ᐟ\ ######################
+#Developed by: Tal Mendelson
+#Purpose: Holds the solutions to Practice 3.
+#Date:08/03/2025
+#################### /ᐠ｡ꞈ｡ᐟ\ ######################
+
+################################################################################################
+# https://gitlab.com/vaiolabs-io/python-script/-/tree/master/01_scripting_intro?ref_type=heads#practice-1
+## Task: 
+# Practice - Number 1 (01_scripting_intro)
+# Create python script file where you:
+# - create variable for holding your name
+# - create variable for holding your age
+# - create variable for holding your pets name
+# - use print() function to print them all.
+# Save the file and run it.
+################################################################################################
+
+# Variable my name
+my_name = "Tal"
+
+# Variable for my age
+my_age = 34
+
+# Pet names
+pet_name = "Bella"
+pet_name2 = "Crookshanks" # Can't leave 2nd cat out :D
+
+# Print them all.
+print("My name is", my_name, ", I have two beautiful cats called", pet_name, "and", pet_name2)
+
+# Cat ASCII 
+cats = '''
+ /\_/\  
+( o.o ) 
+ > ^ <
+'''
+print(cats)
---
+++ Homework/Chapters/Practice_2.py
@@ -0,0 +1,37 @@
+#################### /ᐠ｡ꞈ｡ᐟ\ ######################
+#Developed by: Tal Mendelson
+#Purpose: Holds the solutions to Practice 3.
+#Date:08/03/2025
+#################### /ᐠ｡ꞈ｡ᐟ\ ######################
+
+################################################################################################
+# https://gitlab.com/vaiolabs-io/python-script/-/tree/master/01_scripting_intro?ref_type=heads#practice-2
+## Task: 
+# Practice - Number 2 (01_scripting_intro)
+#Create python script file where you:
+# - create variable for holding your spouse (ife/husband) name
+# - check and print  the type of that variable.
+# - create variable for holding your spouses age
+# - convert age variable to type string
+# - create variable to hold your credit card details (if you wish, you can send them over the email to me: Joking....)
+# - print() them away.
+# Save the file and run it.
+################################################################################################
+
+# Variable for wife name
+waifu_name = "Naama"
+
+# Variable Type
+check_type = type (waifu_name)
+print("waifu_name var type:", check_type)
+
+# Variable wifu age
+waifu_age = 34
+
+# Convert waifu_age from int to str
+waifu_age = str(waifu_age)
+print("waifu_age after converting from int:", type (waifu_age))
+
+# Credit card Variable
+credit_card = "1234-5678-9012-3456"
+print("My no fake at all credit card number:\n",credit_card)
